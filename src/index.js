c5ff73391696940fb5202d740687713678258e2d
advayav <advaya.venbakkam@gmail.com>
2025-08-06 22:00:53 -0400

    added code for the main page to work adn render

--- src/index.js
+++ src/index.js
@@ -1,11 +1,45 @@
-import loadSingleNote from "./singleItem"
-import loadAllTodoItems from "./allLists"
+import loadSingleNote from "./singletodo"
+import loadAllTodoItems from "./alltodos"
+import "./styles/allprojs.css"
 
 function loadAllProjects() {
     const content = document.getElementById("content")
     content.innerHTML = ""
+
+    const allprojs = document.createElement("div")
+    allprojs.className = "project-grid"
+
+    const projList = JSON.parse(localStorage.getItem("projects"))
+    console.log("Project List: ", projList)
+
+    for (let i = 0; i < projList.length; i++) {
+        const project = document.createElement("div")
+        project.className = "project"
+        project.id = `proj-${i}`
+
+        project.innerHTML = `<h3>Project 1</h3>`
+
+        allprojs.appendChild(project)
+
+        
+    }
+    
+    
+    content.appendChild(allprojs)
+
 }
 
+localStorage.clear()
+localStorage.setItem("projects", JSON.stringify(["Project 1"])) 
+
 loadAllProjects()
+
+const newProj = document.getElementById("new-proj")
+    newProj.addEventListener("click", () => {
+        alert("new proj button clicked")
+
+    })
+
+
 
 
